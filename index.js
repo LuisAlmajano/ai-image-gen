7bc9430e74483415ce214f1a99cffb01dc8e1ca8
Luis <luis.almajano@gmail.com>
2023-01-19 18:14:14 +0100

    Added cors package and middleware in Express

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
 
 // Enable body parser
 app.use(express.json());
