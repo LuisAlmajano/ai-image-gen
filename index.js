702707393e0e5b9f2d3ef2e05894fa38814350ed
Luis <luis.almajano@gmail.com>
2023-01-19 18:40:16 +0100

    Slight CORS change in index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// Enable CORS
-app.use(cors());
-
 // Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
